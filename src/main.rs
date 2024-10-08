369a3875fb6993aba30c2462f75cb320a6048c2e
Dan Plubell <Dan.Plubell@coxautoinc.com>
2024-10-08 14:47:57 -0500

    simple player movement

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,22 @@ use prelude::*;
 
 struct State {
     map: Map,
+    player: Player,
 }
 impl State {
     fn new()-> Self {
         Self {
-            map: Map::new()
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
         }
     }
 }
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 fn main()->BError {
