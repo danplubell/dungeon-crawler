db26dd84d6cd6d40308e1b832d3daa4471f85b74
Dan Plubell <Dan.Plubell@coxautoinc.com>
2024-10-08 12:37:10 -0500

    Update map with in_bounds and try_idx

--- src/map.rs
+++ src/map.rs
@@ -18,7 +18,7 @@ impl Map {
     pub fn new()-> Self {
         Self {
             tiles: vec![TileType::Floor; NUM_TILES],
-            
+
             }
         }
     pub fn render(&self, ctx: &mut BTerm){
@@ -34,6 +34,19 @@ impl Map {
                     }
                 }
             }
+        }
+    }
+    pub fn in_bounds(&self, point:Point) -> bool {
+        point.x >= 0 && point.x <SCREEN_WIDTH && point.y >= 0 && point.y <SCREEN_HEIGHT
+    }
+    pub fn can_enter_tile(&self, point:Point)-> bool {
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)]==TileType::Floor
+    }
+    pub fn try_idx(&self, point:Point)->Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        }else {
+            Some(map_idx(point.x, point.y))
         }
     }
 }
